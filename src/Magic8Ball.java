6bd90d82d82966dbc8f40ea5dbdf468945e87d43
Unknown <arieauna.chatmon.2019@student.lhschools.org>
2018-09-11 14:18:59 -0400

    Started declarations of user responses and variables.

---
+++ src/Magic8Ball.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[] args) {
+
+    int testing;
+    Scanner keyboard;
+    boolean userNumber;
+    String userNumberQuestion, userQuestion;
+    boolean userQuestionAnswer;
+    String explanationGreeting;
+
+    keyboard = new Scanner(System.in);
+    explanationGreeting = "";
+    userQuestion = "";
+
+    userQuestionAnswer = keyboard.nextBoolean();
+
+    }
+}
